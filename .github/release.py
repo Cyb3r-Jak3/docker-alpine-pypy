79f137a89a88d40b7c452a3322fb6d9f5cbbd5df
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-12-29 19:19:11 -0500

    Use 7.3.14 for pypy

--- .github/release.py
+++ .github/release.py
@@ -1,21 +1,23 @@
 import os
 import requests
 
+
 def save_file(url: str) -> None:
     """Saves a file from R2"""
     resp = requests.get(url, timeout=300)
     if resp.status_code == 200:
         with open(url.split("/")[-1], "wb") as file:
-                file.write(resp.content)
+            file.write(resp.content)
     else:
         raise Exception(f"Failed to download {url} with status code {resp.status_code}")
+
 
 def main():
     PYPY_BASE = os.environ["PYPY_BASE"]
     PYPY_VERSION = os.environ["PYPY_VERSION"]
-    PYPY_BASES= PYPY_BASE.split(",")
+    PYPY_BASES = PYPY_BASE.split(",")
     ARCHES = ["x86_64", "aarch64"]
-    base_url= "https://pypy.cyberjake.xyz/pypy/{base}/pypy{base}-v{pypy_version}-linux-{arch}-alpine.tar.bz2"
+    base_url = "https://pypy.cyberjake.xyz/pypy/{base}/pypy{base}-v{pypy_version}-linux-{arch}-alpine.tar.bz2"
     for base in PYPY_BASES:
         for arch in ARCHES:
             file_url = base_url.format(
@@ -25,6 +27,7 @@ def main():
             )
             save_file(file_url)
             save_file(f"{file_url}.sig")
+
 
 if __name__ == "__main__":
     main()
---
+++ .github/sign.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import hashlib
+
+BUF_SIZE = 1048576   # lets read stuff in 64kb chunks!
+
+
+def sign(filename: str):
+    subprocess.run(["gpg", "--quiet", "--batch", "--yes", "--detach-sign", filename])
+    sha256sum = hashlib.sha256()
+    with open(sys.argv[1], 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha256sum.update(data)
+    with open(f"{filename}.sha256sum", "w") as f:
+        f.write(f"{sha256sum.hexdigest()}  {filename}")
+
+
+if __name__ == "__main__":
+    sign(sys.argv[1])
